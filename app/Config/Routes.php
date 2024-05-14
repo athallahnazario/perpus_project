14de35641f4af90c8dbc1fd727e199acd031b7e3
athallahnazario <nazarioadrianto@gmail.com>
2024-05-14 11:40:55 +0700

    ngubaah dikit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
+$routes->SetAutoRoute(true);
 
 $routes->get('/login', 'Login::index');
